5c09df63c450499f30d770c61e19522afcf2d061
tori-take <toritakegm@gmail.com>
2025-09-21 09:50:39 +0900

    ユーザ登録実装の修正

--- models/user.js
+++ models/user.js
@@ -11,6 +11,16 @@ const UserSchema = new Schema({
 });
 
 // これをプラグインとして使用すると、UserSchemaにusername, hash, saltフィールドが自動で追加されます。
-UserSchema.plugin(passportLocalMongoose);
+UserSchema.plugin(passportLocalMongoose, {
+    errorMessages: {
+        UserExistsError: 'そのユーザー名はすでに使用されています。',
+        MissingPasswordError: 'パスワードが入力されていません。',
+        AttemptTooSoonError: 'アカウントがロックされています。時間をおいて再度お試しください。',
+        TooManyAttemptsError: 'ログインの試行回数が多すぎます。時間をおいて再度お試しください。',
+        IncorrectPasswordError: 'パスワードまたはユーザー名が正しくありません。',
+        IncorrectUsernameError: 'パスワードまたはユーザー名が正しくありません。',
+        MissingUsernameError: 'ユーザー名が入力されていません。'
+    }
+});
 
 module.exports = mongoose.model('User', UserSchema);
